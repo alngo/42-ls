54ca7b61b4fdb3664b99e70eaa22e72a8af838c2
alngo <alngo@student.42.fr>
2019-09-14 20:30:34 +0200

    feat(Printf): refactor printf.

--- libft/src/printf/ft_printf.c
+++ libft/src/printf/ft_printf.c
@@ -1,24 +1,84 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: alngo <alngo@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/28 16:06:51 by alngo             #+#    #+#             */
-/*   Updated: 2017/06/28 19:06:25 by alngo            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_printf.h"
+#include "libft.h"
 
-int				ft_printf(const char *fmt, ...)
+int					ft_printf(const char *fmt, ...)
 {
-	va_list		args;
-	size_t		ret;
+	int				ans;
+	va_list			ap;
 
-	va_start(args, fmt);
-	ret = ft_xprintf(fmt, &args, 1);
-	va_end(args);
-	return (ret);
+	va_start(ap, fmt);
+	ans = xprintf(&ft_putchar, fmt, ap);
+	va_end(ap);
+	return (ans);
+}
+
+unsigned int				get_flags(const char **fmt)
+{
+	static char				fchar[5];
+	static unsigned char	fbits[6];
+	const char				*t;
+	unsigned char			flags;
+
+	ft_strcpy((char *)fchar, " +-#0");
+	fbits[0] = FSP;
+	fbits[1] = FPL;
+	fbits[2] = FMI;
+	fbits[3] = FNO;
+	fbits[4] = FZE;
+	fbits[5] = 0;
+	flags = 0;
+	while ((t = ft_strchr(fchar, **fmt)) != NULL)
+	{
+		flags |= fbits[t - fchar];
+		(*fmt)++;
+	}
+	printf("flags: %d\n", (int)flags);
+	return (flags);
+}
+
+unsigned int				get_width(const char **fmt, va_list a)
+{
+	unsigned int			width;
+
+	width = 0;
+	if (ft_isdigit(*fmt))
+      width = ft_atoi(*fmt);
+    else if (*fmt == '*') {
+      const int w = va_arg(va, int);
+      if (w < 0) {
+        flags |= FLAGS_LEFT;    // reverse padding
+        width = (unsigned int)-w;
+      }
+      else {
+        width = (unsigned int)w;
+      }
+      format++;
+    }
+}
+
+int					xprintf(void (*pf)(char), const char *fmt, va_list ap)
+{
+	unsigned int	flags;
+	//unsigned int	width;
+	//unsigned int	precision;
+	(void)ap;
+
+	while (*fmt)
+	{
+		if (*fmt == '%')
+		{
+			fmt++;
+			flags = get_flags(&fmt);
+			width = get_width(&fmt, ap);
+			printf("character after flags = %c\n", *fmt);
+		}
+		else
+		{
+			pf(*fmt);
+			fmt++;
+
+		}
+
+	}
+	return (1);
 }
--- src/main.c
+++ src/main.c
@@ -16,5 +16,6 @@ int		main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
+	ft_printf("% +0Ccoucou");
 	return (1);
 }
